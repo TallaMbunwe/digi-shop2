c9c14fd311e5283688afa02b2c97eab0d9603a7d
Eric TALLA <eric.talla@digiplusconsulting.com>
2019-05-05 06:57:25 +0100

    Successfully loaded static resources.

---
+++ digishop2backend/src/main/java/net/dsb/digishop2backend/dao/CategoryDAO.java
@@ -0,0 +1,12 @@
+package net.dsb.digishop2backend.dao;
+
+import java.util.List;
+
+import net.dsb.digishop2backend.dto.Category;
+
+
+public interface CategoryDAO {
+
+	List<Category> list();
+	
+}
---
+++ digishop2backend/src/main/java/net/dsb/digishop2backend/daoimpl/CategoryDAOImpl.java
@@ -0,0 +1,16 @@
+package net.dsb.digishop2backend.daoimpl;
+
+import java.util.List;
+
+import net.dsb.digishop2backend.dao.CategoryDAO;
+import net.dsb.digishop2backend.dto.Category;
+
+public class CategoryDAOImpl implements CategoryDAO {
+
+	@Override
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
